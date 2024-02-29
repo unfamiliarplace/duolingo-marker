b08bc6acffd31db254f8670c671dddffd9290b71
Luke Sawczak <luke@sawczak.com>
2024-02-28 20:30:42 -0500

    restructuring underway

--- src/process.py
+++ src/process.py
@@ -1,65 +1,21 @@
 from __future__ import annotations
 import re
 import os
-import datetime
+import datetime as dt
+from pathlib import Path
 
-FNAME_VARIABLES = './variables.txt'
+PATH_VARIABLES = Path('variables.txt')
+PATH_INPUT = Path('input')
+PATH_OUTPUT = Path('output')
 
-class DuolingoMarker:
-    students: dict[str, Student]
-    student_aliases: dict[str, str]
-    student_xps: dict[str, int]
-    goal: int
-    first_sunday: datetime.date
+FMT_DT_INPUT = '%b %d, %Y %H h %M'
+FMT_DT_OUTPUT = '%Y-%m-%d %H-%M'
 
-    def __init__(self: DuolingoMarker) -> None:
-        self.students = {}
-        self.student_aliases = {}
-        self.student_xps = {}
-        self.goal: 0
-        self.first_sunday = None
+RE_NAME = r'^([-_a-z\(\) ]+) (completed|practiced|tested)'
+RE_XP = r'^\+(\d+) xp'
+RE_DATE = r'^([a-z]+) (\d+), (\d+)'
 
-        self.parse_variables()
-
-        for student in self.student_aliases.values():
-            self.student_xps[student] = 0
-
-    def parse_variables(self: DuolingoMarker):
-        with open(FNAME_VARIABLES, 'r') as f:
-
-            lines = list(filter(None, map(str.strip, f.readlines())))
-            i = 0
-
-            while i < len(lines):
-                line = lines[i]
-
-                k, v = map(str.strip, line.split('::'))
-                
-                if k == 'goal':
-                    self.goal = int(v)
-                
-                elif k == 'first sunday':
-                    y, m, d = map(int, v.split('-'))
-                    self.first_sunday = datetime.date(y, m, d)
-
-                elif k == 'students':
-                    n = int(v)
-                    for j in range(i + 1, n + i + 1):
-                        line = lines[j]
-
-                        duo, canonical = map(lambda s: s.strip().lower(), line.split('::'))
-
-                        if not canonical:
-                            canonical = duo
-
-                        elif canonical == '-':
-                            continue
-
-                        self.student_aliases[duo] = canonical
-                    
-                    i += n
-
-                i += 1
+# Classes
 
 class Student:
     alias: str
@@ -78,73 +34,120 @@ class Practice:
     student: Student
     desc: str
     xp: int
-    date: datetime.datetime
+    date: dt.datetime
 
     def __init__(self: Practice, student: Student, desc: str, xp: int, ds: str) -> None:
         self.student = student
         self.desc = desc
         self.xp = xp
-
-        y, mo, d, h, mi = 0, 0, 0, 0, 0, 0 # TODO
-        self.date = datetime.datetime(y, mo, d, h, mi)
-
+        self.date = dt.strptime(ds, FMT_DT_INPUT)
         self.student.practices.add(self)
 
     def __hash__(self: Practice) -> int:
-        return hash((self.student, self.desc, self.xp, str(datetime)))
-    
-# Begin
+        return hash((self.student, self.desc, self.xp, dt.strftime(FMT_DT_OUTPUT)))
 
-d = DuolingoMarker()
+class DuolingoMarker:
+    students: dict[str, Student]
+    goal: int
+    first_sunday: dt.date
+
+    def __init__(self: DuolingoMarker) -> None:
+        self.students = {}
+        self.goal: 0
+        self.first_sunday = None
+
+    def parse_variables(self: DuolingoMarker):
+        with open(PATH_VARIABLES, 'r') as f:
+
+            lines = list(filter(None, map(str.strip, f.readlines())))
+            i = 0
+
+            while i < len(lines):
+                line = lines[i]
+
+                k, v = map(str.strip, line.split('::'))
+                
+                if k == 'goal':
+                    self.goal = int(v)
+                
+                elif k == 'first sunday':
+                    y, m, d = map(int, v.split('-'))
+                    self.first_sunday = dt.date(y, m, d)
+
+                elif k == 'students':
+                    n = int(v)
+                    for j in range(i + 1, n + i + 1):
+                        line = lines[j]
+
+                        alias, real = map(lambda s: s.strip().lower(), line.split('::'))
+
+                        if not real:
+                            real = alias
+
+                        elif real == '-':
+                            continue
+
+                        s = Student(alias, real)
+                        self.students[alias] = s
+                    
+                    i += n
+
+                i += 1
+
+    def parse_files(self: DuolingoMarker) -> None:        
+        paths = PATH_INPUT.glob('*.txt')
+        for path in paths:
+            if path.stem.startswith('_'):
+                continue
+
+            self.parse_file(path)
+
+    def parse_file(self: DuolingoMarker, path: Path) -> None:
+        state = 0
+        for line in in_file.readlines():
+            line = line.strip().lower()
+
+            if state == 0:
+                m = re.search(RE_NAME, line)
+                if m:
+                    name = m.group(1).strip()
+                    name = d.student_aliases[name]
+                    state = 1
+
+            elif state == 1:
+                m = re.search(RE_XP, line)
+                if m:
+                    xp = int(m.group(1))
+                    state = 2
+
+            elif state == 2:
+                m = re.search(RE_DATE, line)
+                if m:
+                    mo, day, year = list(m.groups())
+
+                    # if date...
+                    d.student_xps[name] += xp
+                    
+                    state = 0
+
+        in_file.close()
+
+    def create_outputs(self: DuolingoMarker) -> None:
+        pass
+
+    def create_output(self: DuolingoMarker) -> None:
+        pass
+
+    
+# Functions
+
+def mark() -> None:
+    d = DuolingoMarker()
+    d.parse_variables()
+    d.parse_files()
+    d.create_outputs()            
 
 # Rewritten up to here
-
-fnames = list(os.walk('input'))[0][2]
-fnames = list(filter(lambda s: not s.startswith('_') and not s.endswith('ini'), fnames))
-latest_in_filename = fnames[-1]
-
-in_filename = input(f'Enter input filename or blank to use latest ({latest_in_filename}): ').strip()
-if not in_filename:
-    in_filename = latest_in_filename
-    
-print(f'Using {in_filename}')
-in_file = open(f'input/{in_filename}', 'r')
-
-# in_filename
-out_filename = f'output/res_{in_filename}'
-
-reg_name = r'^([-_a-z\(\) ]+) (completed|practiced|tested)'
-reg_xp = r'^\+(\d+) xp'
-reg_date = r'^([a-z]+) (\d+), (\d+)'
-
-state = 0
-for line in in_file.readlines():
-    line = line.strip().lower()
-
-    if state == 0:
-        m = re.search(reg_name, line)
-        if m:
-            name = m.group(1).strip()
-            name = d.student_aliases[name]
-            state = 1
-
-    elif state == 1:
-        m = re.search(reg_xp, line)
-        if m:
-            xp = int(m.group(1))
-            state = 2
-
-    elif state == 2:
-        m = re.search(reg_date, line)
-        if m:
-            mo, day, year = list(m.groups())
-
-            # if date...
-            d.student_xps[name] += xp
-            
-            state = 0
-
-in_file.close()
 
 output = ''
 for name in sorted(d.student_xps):
@@ -161,3 +164,11 @@ out_file.close()
 
 print(output)
 input(f'\nSaved to {out_filename}\n\nEnter to quit')
+
+# Run
+
+def run() -> None:
+    mark()
+
+if __name__ == '__main__':
+    run()
